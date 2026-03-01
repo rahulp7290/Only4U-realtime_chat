35fcf2eabb400e50166da8ab7a210cf28c891b03
Rahul Patel <118588944+rahulp7290@users.noreply.github.com>
2026-03-01 15:28:15 +0530

    Update CORS origin for production environment

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const  port = process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://only4u-realtime-chat.onrender.com",
     credentials: true
 }));
 app.use(express.json());
